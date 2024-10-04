75ce5865ed3cdfba1717f508ae3394f8c2ac5f2a
Aniket Gaikwad <aniketgaikwad2305@gmail.com>
2024-10-04 19:28:57 +0530

    Day 12

--- Day01/main.py
+++ Day01/main.py
@@ -1,9 +1,11 @@
 print("Aniket Gaikwad")
 print("23rd September 2024")
 
-print("""I am signing up for Replit's 100 days of Python challenge!
+print(
+    """I am signing up for Replit's 100 days of Python challenge!
 I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!"""
+)
 
 print("I am feeling 😆")
 print("Replit Sucks!")
---
+++ Day12/README.md
@@ -0,0 +1,22 @@
+# Project 12: Debugging
+
+The following code is full of errors. Let's try to fix it.
+
+### Original Code:
+
+```python
+print("100 Days of Code QUIZ")
+print()
+print("How many can you answer correctly?)
+ans1 = ("What language are we writing in?")
+if ans1 == "python":
+  print("Correct")
+else:
+  print("Nope🙈
+ans2 = input("Which lesson number is this?")
+if(ans2>12):
+print("We're not quite that far yet")
+else:
+  print("We've gone well past that!")
+elif(ans2==12):
+  print("That's right!")
