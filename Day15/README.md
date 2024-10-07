fe8034cf9de388580569291f372e393338d96163
Aniket Gaikwad <aniketgaikwad2305@gmail.com>
2024-10-07 23:29:06 +0530

    Day 15

---
+++ Day15/README.md
@@ -0,0 +1,23 @@
+# 👉 Day 15 Challenge: Animal Sound Simulator
+
+## Instructions
+
+Write a program that loops continuously. Inside the loop, ask the user what **animal sound** they want to hear. The program should provide an appropriate response for each animal, and ask if they want to exit or continue.
+
+### Example
+
+```plaintext
+What animal do you want?: Cow
+A cow goes moo.
+
+Do you want to exit?: no
+
+What animal do you want?: A Lesser Spotted Lemur
+Ummm...the Lesser Spotted Lemur goes awooga.
+
+Do you want to exit?: yes
+```
+
+---
+
+## Output:
---
+++ Day15/main.py
@@ -0,0 +1,23 @@
+print("\nAnimal sounds\n")
+
+exit = "no"
+
+while exit == "no":
+    sound = input("What animal sound do you want to hear? ")
+
+    if sound == "cat" or sound == "Cat":
+        print("The cat goes meow!")
+    elif sound == "dog" or sound == "Dog":
+        print("The dog goes Woof!")
+    elif sound == "owl" or sound == "Owl":
+        print("The owl goes hoot hoot!")
+    elif sound == "eagle" or sound == "Eagle":
+        print("What the f is a kilometer???!?!!?!?! raaaahhhh")
+    elif sound == "duck" or sound == "Duck":
+        print("The duck goes quack quack!")
+    elif sound == "goalgetter" or sound == "GoalGetter":
+        print("TTTTTTTTTTTTTTTTTT")
+    else:
+        print("idk that animal 🙁")
+
+    exit = input("\nDo you want to exit? ")
